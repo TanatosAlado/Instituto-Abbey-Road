f971c8e193d3bb505d834897fbd65316eee4d522
Andrés Hosch <andreshosch@hotmail.com>
2023-05-24 22:39:48 -0300

    niv Andres

--- src/app/components/vista-alumno/vista-alumno.component.ts
+++ src/app/components/vista-alumno/vista-alumno.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatTableDataSource } from '@angular/material/table';
+import { Alumno } from 'src/app/models/alumno';
+import { AlumnoService } from 'src/app/services/alumno.service';
 
 @Component({
   selector: 'app-vista-alumno',
@@ -6,5 +10,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./vista-alumno.component.css']
 })
 export class VistaAlumnoComponent {
+  form:FormGroup
+  listAlumnos:Alumno[]=[]
+  dataSource:MatTableDataSource<Alumno>;
+  
+ 
+ 
 
+  constructor(private fb:FormBuilder, private _alumnoService: AlumnoService){ 
+    this.dataSource = new MatTableDataSource(this.listAlumnos);
+  }
+   
+  
+
+  ngOnInit(): void {
+     this.mostrarAlumno()
+}
+
+ mostrarAlumno() {
+  let id=(window.location.pathname).slice(15)
+  this._alumnoService.getStudentById(id).subscribe(doc => {
+    this.listAlumnos.push(doc)   
+  })
+  
+}
+
+  
 }
